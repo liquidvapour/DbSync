9d7e37df03dca9939fb0f63e86f1f6a83308c50e
liquidvapour <liquidvapour@gmail.com>
2014-08-03 10:39:09 +0100

    can now sync and drop schema

--- dbsync.py
+++ dbsync.py
@@ -22,21 +22,6 @@ GET_ALL_SCHEMAS = """
     from dba_users
 """
 
-COMMAND_HELP = """
----------------
-db syncher help
----------------
-syntax: dbsyns.py [command] -s <schema>
-
-arguments
----------
-command 
-    sync (default): syncronises the schema with source control
-    drop: drops the schema.
-"""
-
-COMMANDS = ("sync", "drop")
-
 def does_schema_exist_in_db(cnn, schema):
     cursor = cnn.cursor()
     schemas = [s[0].casefold() for s in cursor.execute(GET_ALL_SCHEMAS).fetchall()]
@@ -50,10 +35,6 @@ def raise_schema_to_version(schema, version):
 def get_all_folders_in(path):
     return [p for p in os.listdir(path) if os.path.isdir(os.path.join(path, p))]
     
-def print_help_and_exit():    
-    print(COMMAND_HELP)
-    sys.exit()
-
 def read_file(filename):
     with open(filename, 'r') as f:
         result = f.read()
@@ -69,12 +50,12 @@ def create_schema(cnn, schema):
 #    cursor.execute(None)
     output, error = runner.run_sql_script('{0}/{1}@{2}'.format(username, password, server), os.path.join('.', schema, 'create.user.sql'))
     print(output)
-    print('error: "{0}".')
+    print('error: "{0}".'.format(error))
     print('schema ({0}) created.'.format(schema))
     
 def process_schema(cnn, schema):
     if not schema in get_all_folders_in('.'):
-        print('Cannot find schema folder for schema: "{0}".'.format(schema))
+        print('Cannot find schema folder for schema: "{0}". Please provide a folder named the same as the schema with all the appropriate scripts'.format(schema))
         return
         
     if not does_schema_exist_in_db(cnn, schema):
@@ -85,39 +66,98 @@ def process_schema(cnn, schema):
 username = 'system'
 password = 'password1234'
 server = 'localhost:1521/XE'    
-    
-def main(argv):
-    schema = ''
-    try:
-        opts, args = getopt.getopt(argv, 'hs:', 'schema=')
-    except getopt.GetoptError:
-        print_help_and_exit()
-    for opt, arg in opts:
-        if opt == '-h':
-            print_help_and_exit()
-        elif opt in ('-s', '-schema'):
-            schema = arg
-    
-    if len(args) > 0:
-        command = args[0].casefold()
-    else:
-        print('no command provided so defaulting to "sync".')
-        command = "sync"
+
+class ArgumentsReader:
+    SYNC = "sync"
+    DROP = "drop"
+    COMMANDS = (SYNC, DROP)
+    COMMAND_HELP = """
+---------------
+db syncher help
+---------------
+syntax: dbsyns.py -s <schema> [command]
+
+arguments
+---------
+command 
+    sync (default): syncronises the schema with source control
+    drop: drops the schema.
+"""
+
+    def __init__(self, argv):
+        print('argv: {0}'.format(argv))
+        schema = ''
+        try:
+            opts, args = getopt.getopt(argv, 'hs:', 'schema=')
+        except getopt.GetoptError:
+            self.print_help_and_exit()
+            
+        print('opts: {0}'.format(opts))
+        print('args: {0}'.format(args))
+        for opt, arg in opts:
+            if opt == '-h':
+                self.print_help_and_exit()
+            elif opt in ('-s', '-schema'):
+                schema = arg
         
-    if not command in COMMANDS: 
-        print('command:"{0}" not one of the valid commands: {1}'.format(command, COMMANDS))
-        print_help_and_exit()
+        if len(args) > 0:
+            self.__command = args[0].casefold()
+        else:
+            print('no command provided so defaulting to "sync".')
+            self.__command = "sync"
+            
+        if not self.__command in ArgumentsReader.COMMANDS: 
+            print('command:"{0}" not one of the valid commands: {1}'.format(self.__command, ArgumentsReader.COMMANDS))
+            self.print_help_and_exit()
+            
+        if not schema:
+            print('no schema provided')
+            self.print_help_and_exit()
+            
+        self.__schema = schema
         
-    if not schema:
-        print('no schema provided')
-        print_help_and_exit()
+    def get_command(self):
+        return self.__command
     
+    def get_schema(self):
+        return self.__schema
+            
+    def print_help_and_exit(self):    
+        print(ArgumentsReader.COMMAND_HELP)
+        sys.exit()
+        
+    def process(self, actions):
+        if self.get_command() in actions:
+            actions[self.get_command()](self)
+        else:
+            print('no action provided for command: "{0}".'.format(self.get_command()))
+        
+
+
+def sync_db(argReader):
     try:
         with cx_Oracle.connect(username, password, server) as db:
-            process_schema(db, schema)
+            process_schema(db, argReader.get_schema())
     except Exception as ex:
         print("Fatal error: {0}".format(ex))
+
+def drop_schema(argReader):
+    schema = argReader.get_schema()
+    print('droping schema: "{0}".'.format(schema))
+    output, error = runner.run_sql_command('{0}/{1}@{2}'.format(username, password, server), 'drop user {0} cascade;'.format(schema))
+    print(output)
         
+def main(argv):
+    
+    argReader = ArgumentsReader(argv)
+    
+    actions = {
+        ArgumentsReader.SYNC: sync_db,
+        ArgumentsReader.DROP: drop_schema
+    }
+    
+    argReader.process(actions)
+    
     
     
 def query_roles(connection):    
--- sqlplusscriptrunner.py
+++ sqlplusscriptrunner.py
@@ -7,3 +7,9 @@ def run_sql_script(connstr, filename):
     sqlplus.stdin.write('@"{0}"'.format(filename))
     return sqlplus.communicate()
     
+def run_sql_command(connstr, script):
+    command = ['sqlplus', '-S', connstr]
+    print(command)
+    sqlplus = Popen(command, stdin=PIPE, stdout = PIPE, stderr=PIPE, universal_newlines = True)
+    sqlplus.stdin.write(script)
+    return sqlplus.communicate()
